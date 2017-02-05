f76cced9561b730ed0da1e842969e360dbd832ad
Baskoro Susanto <baskoro.susanto@gmail.com>
2017-02-05 19:44:18 +0700

    Continued tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
@@ -10,10 +10,7 @@ def index(request):
     return render(request, 'blog/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'blog/detail.html', {'question': question})
 
 def results(request, question_id):
